958202aabf1c4fc558b86607e380b766c0bf4493
root <dancal@empas.com>
2013-06-04 14:45:34 +0900

    first commit

---
+++ bin/_dump_data.php
@@ -0,0 +1,8 @@
+#!/usr/bin/php
+<?php
+
+echo "asfdadsfasd\n";
+
+file_put_contents('php://stderr', 'This text goes to STDERR');
+
+?>
---
+++ bin/mk.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+SCRIPT_PATH=`dirname "$0"`
+ROOTPATH=$SCRIPT_PATH/..
+BINPATH=$ROOTPATH/bin
+DATAPATH=$ROOTPATH/data
+LOGPATH=$ROOTPATH/log
+
+MAX_WORKER=5
+
+server_string="
+1
+2
+"
+server_array=(${server_string//\n/ })
+for i in "${!server_array[@]}"
+do
+
+	SERVER="${server_array[i]}"
+	echo "$i=>${SERVER}"
+	$BINPATH/_dump_data.php $SERVER 2&>$LOGPATH/${i}.log > $DATAPATH/${i}.dat &
+
+	expr_thread=`expr $MAX_WORKER - 1`
+	expr_mod=`expr $i % $MAX_WORKER`
+	if [ "$expr_mod" -eq "$expr_thread" ]
+	then
+		echo "wait"
+		wait
+	fi
+
+done
+
